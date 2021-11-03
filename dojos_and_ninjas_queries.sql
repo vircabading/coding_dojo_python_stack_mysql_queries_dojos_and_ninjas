cea497d1d48ce5d7c76f76f442a7a2f573091c35
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 21:11:29 -0700

    Create 3 Ninjas that belong to each of the three dojos

--- dojos_and_ninjas_queries.sql
+++ dojos_and_ninjas_queries.sql
@@ -17,3 +17,14 @@ WHERE id = 1 or id = 2 or id = 3;
 
 INSERT INTO dojos (name)
 VALUES ('Seattle'), ('San Francisco'), ('Los Angeles');
+
+-- //// Create 3 Ninjas that belong to each of the three dojos ////////
+
+INSERT INTO ninjas (first_name, last_name, age, dojo_id)
+VALUES ('Leonardo', 'Turtle', 16, 4), ('Michaelangelo', 'Turtle', 16, 4), ('Donatelo', 'Turtle', 16, 4);
+
+INSERT INTO ninjas (first_name, last_name, age, ninjas.dojo_id)
+VALUES ('Leonel','Messi',1337, 5), ('Cristiano','Ronaldo', 17, 5), ('Zlatan', 'Ibrahimovic', 37, 5);
+
+INSERT INTO ninjas (first_name, last_name, age, ninjas.dojo_id)
+VALUES ('Max', 'Verstappen', 25, 6), ('Lewis', 'Hamilton', 31, 6), ('Daniel', 'Ricciardo', 28, 6);
