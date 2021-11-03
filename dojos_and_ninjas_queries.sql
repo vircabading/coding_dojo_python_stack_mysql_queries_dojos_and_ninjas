2e54e05fccfa68b3323b54cee01b2769826eb6fb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-02 20:47:14 -0700

    create 3 new dojos

--- dojos_and_ninjas_queries.sql
+++ dojos_and_ninjas_queries.sql
@@ -3,3 +3,7 @@
 -- By:      Virgilio D. Cabading Jr.    Created: November 2, 2021
 -- ///////////////////////////////////////////////////////////////////
 
+-- //// Create 3 new dojos ////////
+
+INSERT INTO dojos (name)
+VALUES ('New York'), ('Boston'), ('Miama');
